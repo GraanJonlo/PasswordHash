6e37c8a21ef515037df04992a7458e27b318cec3
Andrew Grant <andy.a.grant@gmail.com>
2020-07-12 14:37:29 +0100

    test: Added golden master to cover constructor and properties

---
+++ tests/PasswordHash.Tests/GoldenMaster.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace PasswordHash.Tests
+{
+	public class GoldenMaster
+	{
+		[Fact]
+		public void GoldenMasterTest()
+		{
+			var hashAsBase64 = "+MXi0vIqD1LPrPWDkIOmhg==";
+			var hash = Convert.FromBase64String(hashAsBase64);
+			var salt = Convert.FromBase64String("mXO4M+XEh/0NlsY9VibZlA==");
+			var iterations = 100u;
+
+			var sut = new PasswordHash(hash, salt, iterations);
+
+			Assert.Equal(hash, sut.Hash);
+			Assert.Equal(salt, sut.Salt);
+			Assert.Equal(iterations, sut.Iterations);
+			Assert.Equal(hashAsBase64, sut.ToString());
+		}
+	}
+}
